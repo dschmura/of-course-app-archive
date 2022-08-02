8d960fc25774c5a2da44d55f679ee8e09334c9be
Dave Chmura <dschmura@humbledaisy.com>
2022-08-02 11:42:54 -0400

    ADD: TItle helper and support for using locales
    Since this app supprts a "global community of new and returning learners", I decided to start with support for internationalization so it is easy when I decided to add it later.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,15 @@ module ApplicationHelper
     def skip_links
       sanitize @skip_links.collect { |skip_link| link_to "#{skip_link[1]}", "##{skip_link[0]}", class: "skip-to-content-link" }.join(" <span class='sr-only' > or </span>")
     end
+
+      # Returns the full title on a per-page basis.
+  def page_title
+    base_title = (t :site_name)
+    if @page_title.nil?
+      "#{params[:controller].capitalize} | " + base_title
+    else
+      "#{@page_title} | #{base_title}"
+    end
+  end
     
 end
